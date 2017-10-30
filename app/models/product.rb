618e5f239daa9fe24e34cb11fce9ba8bc58f7626
Aleksey <r1ve225@gmail.com>
2017-10-30 16:56:45 +0300

    hw_les3

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+  validates :name, presence: true
+  validates :name, length: { maximum: 30, minimum: 2 }
+  validates :name, uniqueness: true
+  validates :name, format: { with: /\w+/i, message: 'only letters allowed' }
+  validates :img, length: { maximum: 30, minimum: 2 }
+  validates :country, length: { maximum: 30, minimum: 2 }
+  validates :description, length: { maximum: 8000, minimum: 1 }
 end
